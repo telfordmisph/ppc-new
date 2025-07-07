f10b1dfcec1d3dd3bc2e2273ed44874bed1f0e56
telford_mis_ph <telford_mis_ph@astigp.com>
2025-07-07 08:40:34 +0800

    db

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -8,7 +8,9 @@ export default function Dashboard({ tableData, tableFilters }) {
         <AuthenticatedLayout>
             <Head title="Dashboard" />
 
-            <pre>{JSON.stringify(props.emp_data, null, 2)}</pre>
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+
+            {/* <pre>{JSON.stringify(props.emp_data, null, 2)}</pre> */}
         </AuthenticatedLayout>
     );
 }
