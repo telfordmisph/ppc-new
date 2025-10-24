ccb7b4ce36aa59e62326be2cc92dabe0f52aed67
Emem <ememl@emem.com>
2025-10-24 15:48:58 +0800

    feat: Add color constants and toggle button configurations

---
+++ resources/js/Constants/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+  f1Color: "#422ad5",
+  // f2Color: "#7a6ae2",
+  f2Color: "#AE4DFF",
+  // f3Color: "#b3aaee",
+  f3Color: "#FF96EE",
+  accent: "#00d3bb",
+};
---
+++ resources/js/Constants/toggleButtons.js
@@ -0,0 +1,23 @@
+export const TOGGLE_BUTTONS = [
+    {
+        key: "f1",
+        label: "F1",
+        activeClass: "bg-f1color/50 border-f1color text-base-content",
+        inactiveClass:
+            "border-f1color text-f1color hover:bg-f1color/50-content",
+    },
+    {
+        key: "f2",
+        label: "F2",
+        activeClass: "bg-f2color/50 border-f2color text-base-content",
+        inactiveClass:
+            "border-f2color text-f2color hover:bg-f2color/50-content",
+    },
+    {
+        key: "f3",
+        label: "F3",
+        activeClass: "bg-f3color/50 border-f3color text-base-content",
+        inactiveClass:
+            "border-f3color text-f3color hover:bg-f3color/50-content",
+    },
+];
